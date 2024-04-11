f95c22e82b7eb307228dcc29230a1b222d4402fc
naviwatch <146077263+naviwatch@users.noreply.github.com>
2024-04-11 18:31:34 +0800

    Update Goodbye Menu.lua

--- Goodbye Menu.lua
+++ Goodbye Menu.lua
@@ -3,8 +3,7 @@ local mod = get_mod("Goodbye Menu")
 -- Skip the Menu
 GameSettingsDevelopment.skip_start_screen = true
 
--- Reverted because opening the menu will cause VMF to spam safe hook errors
---[[  
+
 -- Get the ui settings
 local settings = require("scripts/ui/views/ingame_ui_settings")
 local transitions = settings.transitions
@@ -12,4 +11,4 @@ local transitions = settings.transitions
 -- Override the original quit button to directly quit the game
 transitions.do_return_to_title_screen = transitions.quit_game
 transitions.return_to_title_screen = transitions.quit_game
-]]
+
